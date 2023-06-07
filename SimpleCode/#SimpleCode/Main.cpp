dbc1c7759ccf3c5f40ab15e5e25c5384e4033f25
pytocripe <marat.moroz14@gmail.com>
2023-06-07 14:44:13 +0300

    Third program. Sum, product and arith mean of three integers

--- SimpleCode/#SimpleCode/Main.cpp
+++ SimpleCode/#SimpleCode/Main.cpp
@@ -1,25 +1,22 @@
 #include<iostream>
 using namespace std;
 
-// First homework assignment from #SimpleCode
-
-/*
-
-\\ - print the '/' character
-\' - print single quotes
-\" - print binary quotes
-\n - move to the next line
-\t - tabulation
-
-*/
+// Second homework assignment from Simple Code.
 
 
 void main()
-{
-   cout << "\t\t\t There is a house in New Orleans \n\t\t\t They call the \"Rising Sun\" \n\t\t\t And it's been the ruin of many a poor boy \n\t\t\t And God, I know I'm one" << endl;
-   cout << "\n\t\t\t My mother was a tailor \n\t\t\t She sewed my new blue jeans \n\t\t\t My father was a gamblin' man \n\t\t\t Down in New Orleans" << endl;
-   cout << "\n\t\t\t Now the only thing a gambler needs \n\t\t\t Is a suitcase and a trunk \n\t\t\t And the only time he'll be satisfied \n\t\t\t Is when he's all drunk" << endl;
-   cout << "\n\t\t\t Oh, mother, tell your children \n\t\t\t Not to do what I have done \n\t\t\t Spend your lives in sin and misery \n\t\t\t In the \"House of the Rising Sun\"" << endl;
-   cout << "\n\t\t\t Well, I got one foot on the platform \n\t\t\t Not to do what I have done \n\t\t\t I'm goin' back to New Orleans \n\t\t\t To wear that ball and chain" << endl;
-   cout << "\n\t\t\t Well, there is a house in New Orleans \n\t\t\t \"They call the Rising Sun\" \n\t\t\t And it's been the ruin of many a poor boy \n\t\t\t And God, I know I'm one" << endl;
+{ 
+	int firstNumber, secondNumber, thirdNumber;
+
+	cout << "Write first number: ";
+	cin >> firstNumber;
+	cout << "Write second number: ";
+	cin >> secondNumber;
+	cout << "Write third number: ";
+	cin >> thirdNumber;
+
+	cout << "Sum of these three numbers is: " << (firstNumber + secondNumber + thirdNumber) << endl;
+	cout << "Product of these three numbers is: " << (firstNumber * secondNumber * thirdNumber) << endl;
+	cout << "Arithmetic mean of these three numbers is: " << ((firstNumber + secondNumber + thirdNumber) / 3) << endl;
+
 }
