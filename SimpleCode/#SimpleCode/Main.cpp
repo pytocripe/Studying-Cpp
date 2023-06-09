edae6b8cc41d16d6ad22e0a38f4588dd4bcf4482
pytocripe <marat.moroz14@gmail.com>
2023-06-09 13:29:01 +0300

    Fourth program. Check the number even or odd

--- SimpleCode/#SimpleCode/Main.cpp
+++ SimpleCode/#SimpleCode/Main.cpp
@@ -1,22 +1,21 @@
 #include<iostream>
 using namespace std;
 
-// Second homework assignment from Simple Code.
-
+// Third homework assignment from #SimpleCode
 
 void main()
-{ 
-	int firstNumber, secondNumber, thirdNumber;
+{
+	int CheckNumber;
+	cout << "Write the number you want to check: ";
+	cin >> CheckNumber; 
 
-	cout << "Write first number: ";
-	cin >> firstNumber;
-	cout << "Write second number: ";
-	cin >> secondNumber;
-	cout << "Write third number: ";
-	cin >> thirdNumber;
-
-	cout << "Sum of these three numbers is: " << (firstNumber + secondNumber + thirdNumber) << endl;
-	cout << "Product of these three numbers is: " << (firstNumber * secondNumber * thirdNumber) << endl;
-	cout << "Arithmetic mean of these three numbers is: " << ((firstNumber + secondNumber + thirdNumber) / 3) << endl;
+	if ((CheckNumber % 2) == 0)
+	{
+		cout << "Number is even!"; 
+	}
+	else
+	{
+		cout << "Number is odd!";
+	}
 
 }
